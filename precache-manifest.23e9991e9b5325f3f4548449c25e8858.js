94863426e39032df15cb7cd3657ab9cbc9f5e7ec
Khanh <khanhtx@nal.vn>
2019-01-24 18:44:05 +0700

    change to react simple treetable

---
+++ precache-manifest.23e9991e9b5325f3f4548449c25e8858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "238c57f51ee8d03eab96",
+    "url": "/static/js/main.238c57f5.chunk.js"
+  },
+  {
+    "revision": "05a5deec86427ef8f2e6",
+    "url": "/static/js/1.05a5deec.chunk.js"
+  },
+  {
+    "revision": "238c57f51ee8d03eab96",
+    "url": "/static/css/main.fe0f910b.chunk.css"
+  },
+  {
+    "revision": "27cf31320d3d9f33bfc5893405fa74d2",
+    "url": "/index.html"
+  }
+];
